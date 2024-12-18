413ddfa2a30f892f934b7f170b239f013e6f4908
Louise Aldridge <louisealdridge88@gmail.com>
2024-12-18 16:02:57 +0000

    Active nav class added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import { Link, NavLink } from "react-router"
 import { FaTerminal } from "react-icons/fa"
 
 export default function Header() {
+
     return (
         <nav>
             <Link className="logo" to="/"> <FaTerminal /> louise.codes</Link>
             <div className="nav-links">
-                <NavLink to="/projects">Projects</NavLink>
-                <NavLink to="/about">About</NavLink>
+                <NavLink to="/projects" className={({ isActive }) => (isActive ? 'nav-active' : "")} >Projects</NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-active' : "")} >About</NavLink>
             </div>
         </nav>
     )

c468469217c27a486e038987696d284e1db3a5ab
Louise Aldridge <louisealdridge88@gmail.com>
2024-12-18 14:28:10 +0000

    Creating navigation for parent pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+
+import Layout from "./components/Layout"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects/Projects"
+import About from "./pages/About/About"
+
+
 function App() {
 
   return (
-    <p>empty app</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
 
-    //React Router will go here
+
+    </BrowserRouter>
 
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
+import { Link, NavLink } from "react-router"
+
 export default function Header() {
     return (
-        <h1>This is the header</h1>
+        <nav>
+            <Link to="/">louise.codes</Link>
+            <div>
+                <NavLink to="/projects">Projects</NavLink>
+                <NavLink to="/about">About</NavLink>
+            </div>
+        </nav>
     )
 }

42d758a9192622f7eb782dc507fcb9732c2057ca
Louise Aldridge <louisealdridge88@gmail.com>
2024-12-19 15:10:47 +0000

    Styling and creating home page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,9 +8,9 @@ export default function Layout() {
     return (
         <>
             <Header />
-            <main>
-                <Outlet />
-            </main>
+
+            <Outlet />
+
             <Footer />
         </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
 export default function Home() {
     return (
-        <h1>This is the home page</h1>
+        <>
+            <header>
+                <div className="hero-text">
+                    <h1> Hello, I'm Louise</h1>
+                    <p> I'm a Frontend Developer with a strong eye for detail and a passion for accessibility. With three
+                        years
+                        of experience in eCommerce and two in Marketing, I bring a well-rounded skill set, including design
+                        expertise, task management, and problem-solving abilities. </p>
+                </div>
+                <img className="hero-img" src="./assets/louise-profile.png" alt="Profile image of Louise smiling" />
+
+            </header>
+        </>
     )
 }

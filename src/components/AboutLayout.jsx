7a4b4aa3ace85b835903be4c68d31191cb5eb49b
Louise Aldridge <louisealdridge88@gmail.com>
2024-12-19 13:46:31 +0000

    Fixed bug with active styles

--- src/components/AboutLayout.jsx
+++ src/components/AboutLayout.jsx
@@ -5,7 +5,7 @@ export default function AboutLayout() {
         <>
             <h1>This is the About page</h1>
             <nav>
-                <NavLink to="." className={({ isActive }) => (isActive ? 'nav-active' : "")}>Skills</NavLink>
+                <NavLink to="." end className={({ isActive }) => (isActive ? 'nav-active' : "")}>Skills</NavLink>
                 <NavLink to="job-history" className={({ isActive }) => (isActive ? 'nav-active' : "")}>Job History</NavLink>
                 <NavLink to="education" className={({ isActive }) => (isActive ? 'nav-active' : "")}>Education</NavLink>
             </nav>

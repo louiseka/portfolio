eaaf8e7230b0643c8bac6d13d5bd5e4ba9846538
Louise Aldridge <louisealdridge88@gmail.com>
2024-12-19 11:49:53 +0000

    Created route for project/:id

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects/Projects"
+import ProjectDetails from "./pages/Projects/ProjectDetails"
 import AboutLayout from "./components/AboutLayout"
 import Skills from "./pages/About/Skills"
 import JobHistory from "./pages/About/JobHistory"
@@ -17,13 +18,12 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<ProjectDetails />} />
           <Route path="about" element={<AboutLayout />}>
             <Route index element={<Skills />} />
             <Route path="education" element={<Education />} />
             <Route path="job-history" element={<JobHistory />} />
           </Route>
-
-
         </Route>
       </Routes>
 
--- src/pages/Projects/ProjectDetails.jsx
+++ src/pages/Projects/ProjectDetails.jsx
@@ -1,4 +1,11 @@
+import { useParams } from "react-router"
+
+
+
 export default function ProjectDetails() {
+
+    const { id } = useParams()
+
     return (
         <h1>This is the Project Details page</h1>
     )

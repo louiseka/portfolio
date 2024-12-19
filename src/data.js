0fd173250a1241cafe3aeecdcf601820969910e5
Louise Aldridge <louisealdridge88@gmail.com>
2024-12-19 11:49:40 +0000

    Creating template data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const projectData = [
+    {
+        id: 1,
+        projectName: "Project 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est eros, ultricies quis cursus sed, porttitor ullamcorper leo. Suspendisse quam enim, malesuada sed suscipit quis, maximus ac libero. Donec porta est eget mauris hendrerit efficitur. Vestibulum elementum semper gravida."
+    },
+    {
+        id: 2,
+        projectName: "Project 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est eros, ultricies quis cursus sed, porttitor ullamcorper leo. Suspendisse quam enim, malesuada sed suscipit quis, maximus ac libero. Donec porta est eget mauris hendrerit efficitur. Vestibulum elementum semper gravida."
+    },
+    {
+        id: 3,
+        projectName: "Project 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est eros, ultricies quis cursus sed, porttitor ullamcorper leo. Suspendisse quam enim, malesuada sed suscipit quis, maximus ac libero. Donec porta est eget mauris hendrerit efficitur. Vestibulum elementum semper gravida."
+    },
+    {
+        id: 4,
+        projectName: "Project 4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est eros, ultricies quis cursus sed, porttitor ullamcorper leo. Suspendisse quam enim, malesuada sed suscipit quis, maximus ac libero. Donec porta est eget mauris hendrerit efficitur. Vestibulum elementum semper gravida."
+    },
+    {
+        id: 5,
+        projectName: "Project 5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est eros, ultricies quis cursus sed, porttitor ullamcorper leo. Suspendisse quam enim, malesuada sed suscipit quis, maximus ac libero. Donec porta est eget mauris hendrerit efficitur. Vestibulum elementum semper gravida."
+    },
+
+] 

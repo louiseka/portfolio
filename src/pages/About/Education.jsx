bc9c3088b8739b8ced887a5a2e1014df6ee0ee6a
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-02 15:39:12 +0000

    Styling nested pages

--- src/pages/About/Education.jsx
+++ src/pages/About/Education.jsx
@@ -2,7 +2,18 @@ export default function Education() {
     return (
         <section className="education-section">
             <h2>Education</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae libero et sapien fermentum maximus. Aenean vitae libero et sapien fermentum maximus.  </p>
+            <div className="details-text">
+                <h3>University of Sussex, Master of Arts - Digital Media</h3>
+                <ul>
+                    <li>2018 - 2019, Grade: Distinction </li>
+                </ul>
+            </div>
+            <div className="details-text">
+                <h3>University of Sussex, Master of Arts - Digital Media</h3>
+                <ul>
+                    <li>2018 - 2019, Grade: Distinction </li>
+                </ul>
+            </div>
         </section>
 
     )

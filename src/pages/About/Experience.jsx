15dfd143e127eed8051eb205f2419ab3446b334b
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-03 11:29:41 +0000

    Styled experience tab

--- src/pages/About/Experience.jsx
+++ src/pages/About/Experience.jsx
@@ -2,31 +2,28 @@ export default function Experience() {
     return (
         <section className="job-section">
             <h2>Experience</h2>
-            <div className="details-text">
+            <div className="details-section-1">
                 <h3>AbilityNet </h3>
-                <h4>Digital Marketing Assistant</h4>
-                <p>June 2022 - May 2024</p>
+                <p className="detail-sub">Digital Marketing Assistant</p>
+                <p className="small-text"> June 2022 - May 2024 </p>
                 <ul>
                     <li>Raised awareness about disability and digital accessibility by creating content for social media, email campaigns, podcast channels, and the charity’s website. </li>
                     <li>Adjusted the HTML and CSS of a new embedded newsletter sign-up form to align with the charity’s website styling.</li>
                 </ul>
             </div>
-            <div className="details-text">
+            <div className="details-section-2">
                 <h3>Qvis Lighting & Security</h3>
-                <h4>eCommerce Team Leader</h4>
-                <p>June 2021 - June 2022</p>
+                <p className="detail-sub">eCommerce Team Leader </p>
+                <p className="small-text">June 2021 - June 2022</p>
                 <ul>
                     <li>Collaborated closely with the overseas development team to troubleshoot and resolve issues within the app and website, based on user feedback and manual testing.</li>
                     <li> Managed and resolved website/app related tickets on the team’s service desk. </li>
                     <li>Followed wireframe designs to create and update pages on the company's website and app.</li>
                     <li>Used page builders, HTML, and CSS to create user-friendly web pages.</li>
                     <li>Re-developed and executed a successful email automation and campaign strategy, resulting in increased customer engagement and sales.</li>
-
                 </ul>
-            </div>
-            <div className="details-text">
-                <h4>Website Administrator</h4>
-                <p>October 2019 - June 2021</p>
+                <p className="detail-sub">Website Administrator</p>
+                <p className="small-text">October 2019 - June 2021</p>
                 <ul>
                     <li>Managed website content by regularly updating and adding new products, brands, categories, and informative content. </li>
                     <li>Used HTML, CSS, and page builders to create user-friendly web pages.</li>

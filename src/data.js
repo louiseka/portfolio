100d97230a6ee3b3eb32232ea682bec719781cb3
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-06 13:09:35 +0000

    Links to each project detail page created

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const projectData = [
     {
-        id: 1,
+        id: "1",
         name: "Quizzical",
         shortDescription: "A general knowledge quiz, created using React",
         longDescription: "Quizzical is a five-question general knowledge quiz, using the Open Trivia Database API. I chose React for this project to create reusable components for each question and answer, manage and track state throughout the quiz, and leverage useEffect to execute code based on dependencies.",
@@ -14,7 +14,7 @@ export const projectData = [
 
     },
     {
-        id: 2,
+        id: "2",
         name: "Film Watchlist",
         shortDescription: "A Film Watchlist, enabling users to search for films they want to watch.",
         longDescription: "This Film Watchlist leverages the Open Movie Database API, enabling users to search for films they want to watch. Users can add films to a watchlist or remove them as needed, with all data saved to and removed from their local storage. I chose JavaScript for this project for easy API handling, use of localStorage, and to enhance user experience.",
@@ -27,7 +27,7 @@ export const projectData = [
         cardLayout: "wide"
     },
     {
-        id: 3,
+        id: "3",
         name: "Recipe Roulette",
         shortDescription: "A random meal generator to help you choose tonight's dinner.",
         longDescription: "Recipe Roulette' is a random meal generator. Choose a cuisine, click 'Get Random Recipe,' and a recipe will appear in a pop-up with an image and a link to the full recipe. Don't like it? Close the pop-up and try again! I used JavaScript for this project to make use of a flexible dataset, to be able to use Math.random to generate random recipes from an array, and to handle user interactions easily. ",
@@ -40,7 +40,7 @@ export const projectData = [
         cardLayout: "wide"
     },
     {
-        id: 4,
+        id: "4",
         name: "Dish Diaries",
         shortDescription: "A solo project that was created as a final presentation piece for Code First Girls Kickstarter course.",
         longDescription: "Dish Diaries was a solo project that was created as a final presentation piece for Code First Girls Coding Kickstarter Web Development course. Dish Diaries is an accessible alternative to those keyword and ad stuffed recipe websites that we have all experienced. ",
@@ -53,7 +53,7 @@ export const projectData = [
         cardLayout: "normal"
     },
     {
-        id: 5,
+        id: "5",
         name: "Colour Scheme Generator",
         shortDescription: "Users can select a colour and a colour scheme from a dropdown menu to generate a palette.",
         longDescription: "I've created a Colour Scheme Picker using 'The Color API.' Users can select a colour and a colour scheme from a dropdown menu to generate a palette of five colours based on the chosen scheme.I chose to use JavaScript, so that I could respond to user’s inputs via event listeners, retrieve data from the The Color API efficiently with fetch(), and to manipulate the DOM to display the generated palettes. ",
@@ -66,7 +66,7 @@ export const projectData = [
         cardLayout: "normal"
     },
     {
-        id: 6,
+        id: "6",
         name: "BoredBot",
         shortDescription: "A random idea generator for when you're bored.",
         longDescription: "BoredBot is an idea generator powered by the 'Bored API'. Click the button to generate a random idea. Don't like the idea? Just keep clicking until you find one you love! I chose JavaScript for this project to handle button clicks with event listeners, fetch data efficiently from the Bored API, and dynamically manipulate the DOM to display the generated ideas.",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,9 @@
 import { Link } from "react-router"
 import { FaArrowRight } from "react-icons/fa"
 import { projectData } from "../data"
+import Projects from "./Projects/Projects"
 
 export default function Home() {
-
-    const projectElements = projectData.map(project => {
-
-        const isWide = project.cardLayout === "wide"
-
-        const projectName = project.name
-        const projectShortDesc = project.shortDescription
-        const projectThumbnail = project.thumbnailImg
-        const projectGridPosition = `${project.gridPosition} card `
-
-
-        return (
-            <div key={project.id} className={projectGridPosition}>
-                <div className={isWide ? "wide-inner-content" : ""}>
-                    <img className={isWide ? "wide-grid-img" : "normal-grid-img"} src={projectThumbnail} />
-                    <div className={isWide ? "wider-inner-text" : ""}>
-                        <h2>{projectName}</h2>
-                        <p>{projectShortDesc}</p>
-                    </div>
-                </div>
-            </div >
-        )
-    })
-
-
-
-
-
-
-
-
-
-
 
 
     return (
@@ -51,16 +19,7 @@ export default function Home() {
                 </div>
                 <img className="profile-img" src="./assets/louise-profile.png" alt="Profile image of Louise smiling" />
             </header>
-            <div className="projects-grid">
-                <h2>My portfolio of projects</h2>
-                <p className="sub-text"> I'm a Frontend Developer with a strong eye for detail and a passion for accessibility. With three
-                    years
-                    of experience in eCommerce and two in Marketing, I bring a well-rounded skill set, including design
-                    expertise, task management, and problem-solving abilities. </p>
-                <div className="wrapper">
-                    {projectElements}
-                </div>
-            </div>
+            <Projects />
         </>
     )
 }

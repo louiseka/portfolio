efb618216a8a0ba0f18bfb6c8ce6e1b0d3a6f70c
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-07 15:44:42 +0000

    Footer created and styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,38 @@
+import { FaTerminal, FaLinkedin, FaTwitterSquare, FaGithubSquare, FaEnvelope } from "react-icons/fa"
+import { Link } from "react-router"
+
 export default function Footer() {
     return (
-        <h1>This is the footer</h1>
+        <footer>
+            <div className="rights-decl">
+                <p className="footer-logo"> <FaTerminal /> louise.codes</p>
+            </div>
+            <div className="footer-container">
+
+                <div className="footer-about">
+                    {/* <p className="footer-logo"> <FaTerminal /> louise.codes</p> */}
+                    <p className="small-title"> Hello, I'm Louise</p>
+                    <p>I'm a Frontend Developer with a strong eye for detail and a passion for accessibility. I bring a well-rounded skill set, including design expertise, task management, and problem-solving abilities. </p>
+
+                </div>
+
+                <div className="footer-socials">
+                    <p className="small-title"> Follow my coding journey</p>
+                    <p>I regularly share my latest projects and learning milestones on my social accounts. You can find me on LinkedIn, Twitter (reluctantly known as X), and GitHub. If you'd like to get in touch, feel free to send me an email. </p>
+                    <div className="social-icons-container">
+                        <Link to="https://www.linkedin.com/in/louiseka/" target="_blank"> <FaLinkedin className="social-icon" /></Link>
+                        <Link to="https://x.com/louiseka_codes" target="_blank"><FaTwitterSquare className="social-icon" /></Link>
+                        <Link to="https://github.com/louiseka" target="_blank"><FaGithubSquare className="social-icon" /></Link>
+                        <Link to="mailto:louisealdridge88@gmail.com" target="_blank"> <FaEnvelope className="social-icon" /></Link>
+                    </div>
+                </div>
+
+            </div>
+            <div className="rights-decl">
+                <p className="small-text">All rights reserved. © 2025 Louise Aldridge. </p>
+            </div>
+
+        </footer >
+
     )
 }

6e7f65080719868f873d29b8217613974bce227a
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-08 15:39:42 +0000

    Updates to styling and content

--- src/components/AboutLayout.jsx
+++ src/components/AboutLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, NavLink, Link } from "react-router"
-import { FaArrowRight } from "react-icons/fa"
+import { FaFileDownload } from "react-icons/fa"
 
 export default function AboutLayout() {
     return (
@@ -10,18 +10,12 @@ export default function AboutLayout() {
                     <div className="hero-text">
                         <h1>Let me introduce myself</h1>
                         <p>Hi, I’m Louise - a Frontend Developer with a strong eye for detail and a passion for accessibility. With three years of experience in eCommerce and two in Marketing, I bring a well-rounded skill set, including design expertise, task management, and problem-solving abilities. I’m self-motivated, detail-oriented, and committed to creating user-friendly, accessible digital experiences. </p>
-                        <p>My passion for coding was reignited at AbilityNet, where I discovered the importance of digital accessibility and became a IAAP Certified Professional in Accessibility Core Competencies (CPACC). This led me to self-teach and hone my frontend development skills in HTML, CSS, JavaScript (ES6, Async), React, git, and GitHub. </p>
+                        <p>My passion for coding was reignited at AbilityNet, where I discovered the importance of digital accessibility and became a IAAP Certified Professional in Accessibility Core Competencies (CPACC). This led me to self-teach and hone my frontend development skills in HTML, CSS, JavaScript (ES6, Async), React, git, and GitHub. <a className="medium-cta-link" href="/assets/CV-Louise-Aldridge.pdf" target="_blank">Download a copy of my CV <FaFileDownload className="icon" /> </a> </p>
 
                     </div>
 
                     <img className="profile-img" src="/assets/louise-profile.png" alt="Profile image of Louise smiling" />
                 </div>
-
-
-
-
-
-
 
                 <nav className="about-nav">
                     <NavLink to="." end className={({ isActive }) => (isActive ? 'nav-active' : "")}>Skills</NavLink>
--- src/pages/About/Skills.jsx
+++ src/pages/About/Skills.jsx
@@ -19,6 +19,7 @@ export default function Skills() {
                 <p className="green-label">Bootstrap</p>
                 <p className="purple-label">Magento</p>
                 <p className="pink-label">SEO</p>
+                <p className="green-label">Responsive Design</p>
             </div>
             <Link className="medium-cta-link" to="https://github.com/louiseka" target="_blank">Visit my GitHub <FaArrowRight className="icon" /></Link>
             <Link className="medium-cta-link" to={"/#projects"}>Explore my portfolio of projects <FaArrowRight className="icon" /></Link>

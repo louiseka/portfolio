096c768f9f025335557643d30ccbf82d977e549d
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-08 16:23:36 +0000

    Logic added to remove hover from active links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,12 @@ import { Link, NavLink } from "react-router"
 import { FaTerminal } from "react-icons/fa"
 
 export default function Header() {
-
-
-
     return (
-        <nav id="navbar">
-            <Link className="logo" to="/"> <FaTerminal /> louise.codes</Link>
+        <nav>
+            <NavLink className={({ isActive }) => (isActive ? 'logo active' : "logo")} to="/"> <FaTerminal /> louise.codes</NavLink>
             <div className="nav-links">
                 {/* <NavLink to="/projects" className={({ isActive }) => (isActive ? 'nav-active' : "")} >Projects</NavLink> */}
-                <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-active' : "")} >About</NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-active active' : "")} >About</NavLink>
             </div>
         </nav>
     )

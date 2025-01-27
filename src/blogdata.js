969fa0253bbccda0549f3fabb13251e71f4e5624
Louise Aldridge <louisealdridge88@gmail.com>
2025-01-27 15:22:44 +0000

    Creating blog data

---
+++ src/blogdata.js
@@ -0,0 +1,12 @@
+export const blogData = [
+    {
+        id: "1",
+        name: "Kickstarting a new project",
+        shortDescription: "As I continue to develop my frontend development skills, I've recently started thinking about a new solo project to work on...",
+        longDescription: "As I continue to develop my frontend development skills, I've recently started thinking about a new solo project to work on. Pinpoint is a goal tracker...",
+        thumbnailImg: "/assets/blog-imgs/moodboard.png",
+        gridPosition: "one",
+        cardLayout: "normal",
+        urlSlug: "blog-one"
+    }
+]

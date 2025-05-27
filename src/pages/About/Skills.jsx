e9d25b5798c75e85e52b0e26fe0849e4e78fa9e2
Louise Aldridge <louisealdridge88@gmail.com>
2025-05-27 11:33:02 +0100

    Updating skills section

--- src/pages/About/Skills.jsx
+++ src/pages/About/Skills.jsx
@@ -8,6 +8,9 @@ export default function Skills() {
                 <h2>Skills</h2>
                 <div className="skills-container">
                     <p className="green-label">React.js</p>
+                    <p className="purple-label">Tailwind</p>
+                    <p className="pink-label">TypeScript</p>
+                    <p className="green-label">ARIA</p>
                     <p className="purple-label">JavaScript</p>
                     <p className="pink-label">HTML</p>
                     <p className="green-label">CSS</p>
@@ -22,6 +25,7 @@ export default function Skills() {
                     <p className="purple-label">Magento</p>
                     <p className="pink-label">SEO</p>
                     <p className="green-label">Responsive Design</p>
+                    <p className="purple-label">Accessibility</p>
                 </div>
                 <Link className="medium-cta-link" to="https://github.com/louiseka" target="_blank" title="Louise Aldridge's GitHub profile">Visit my GitHub <FaArrowRight className="icon" /></Link>
                 <Link className="medium-cta-link" to={"/#projects"} title="An overview page of Louise Aldridge's projects">Explore my portfolio of projects <FaArrowRight className="icon" /></Link>

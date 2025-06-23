f0c4402d259d4a17a09366a73c6cd18b195db6d6
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-23 14:54:20 +0100

    Correcting fucntion name

--- src/pages/Blogs/BlogThree.jsx
+++ src/pages/Blogs/BlogThree.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 import SyntaxHighlighter from "react-syntax-highlighter"
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
-export default function BlogTwo() {
+export default function BlogThree() {
 
     useEffect(() => {
         window.scrollTo(0, 0);

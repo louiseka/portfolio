236577ec09a5892ae796e5e8bdc64fdb06481afb
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-03 14:35:52 +0100

    Adding blog data

--- src/blogdata.js
+++ src/blogdata.js
@@ -1,70 +1,87 @@
 export const blogData = [
-    {
-        id: "11",
-        name: "Kickstarting a new project",
-        date: "23 January 2025",
-        shortDescription: "As I continue to develop my frontend development skills, I've recently started thinking about a new solo project to work on...",
-        thumbnailImg: "/assets/blog-imgs/moodboard.png",
-        altText: "A lady pinning items to a moodboard.",
-        gridPosition: "one",
-        cardLayout: "normal",
-        urlSlug: "kickstarting-new-project"
-    },
+  {
+    id: "11",
+    name: "Kickstarting a new project",
+    date: "23 January 2025",
+    shortDescription:
+      "As I continue to develop my frontend development skills, I've recently started thinking about a new solo project to work on...",
+    thumbnailImg: "/assets/blog-imgs/moodboard.png",
+    altText: "A lady pinning items to a moodboard.",
+    gridPosition: "one",
+    cardLayout: "normal",
+    urlSlug: "kickstarting-new-project",
+  },
 
-    {
-        id: "12",
-        name: "Designing Pinpoint",
-        date: "31 January 2025",
-        shortDescription: "Designing lays a strong foundation for user experience, helping to define a project's needs and features. This week I jumped into Figma...",
-        thumbnailImg: "/assets/blog-imgs/wireframe.png",
-        altText: "A notepad with a hand drawn wireframe of a website.",
-        gridPosition: "two",
-        cardLayout: "normal",
-        urlSlug: "designing-pinpoint"
-    },
-    {
-        id: "13",
-        name: "It's fine to prop drill",
-        date: "3 March 2025",
-        shortDescription: "It’s fine to prop drill - at least when building Pinpoint. Since it’s a small application with only one or two layers...",
-        thumbnailImg: "/assets/blog-imgs/Drilling.png",
-        altText: "A person using a drill.",
-        gridPosition: "three",
-        cardLayout: "normal",
-        urlSlug: "fine-prop-drilling"
-    },
-    {
-        id: "14",
-        name: "From Marketing to Frontend Development",
-        date: "23 June 2025",
-        shortDescription: "It’s been just over a year since leaving my marketing role and embarking on a career switch to frontend development, and I wanted to take some time to reflect...",
-        thumbnailImg: "/assets/blog-imgs/cat-code.png",
-        altText: "A cat sitting infront of a Mac screen with code on it.",
-        gridPosition: "four",
-        cardLayout: "wide",
-        urlSlug: "marketing-to-frontend-development"
-    },
-    {
-        id: "15",
-        name: "Bootcamp Week 1 – Learning Agile and Pair Programming",
-        date: "19 September 2025",
-        shortDescription: "This week, I completed the first week of TechNative Digital’s Software Development – Tech Finishing School Bootcamp, a 12-week program designed to take coding skills to a job-ready level.",
-        thumbnailImg: "/assets/blog-imgs/pears.png",
-        altText: "Two pears",
-        gridPosition: "five",
-        cardLayout: "wide",
-        urlSlug: "bootcamp-week1-agile-and-pair-programming"
-    },
-    {
-        id: "16",
-        name: "Bootcamp Week 2 – CSS Naming Conventions and Code Conflicts",
-        date: "26 September 2025",
-        shortDescription: "This week’s bootcamp focus was on CSS and pair programming. We started with a solo CSS tutorial, which served as a great refresher...",
-        thumbnailImg: "/assets/blog-imgs/conflict-deer.png",
-        altText: "Two deers hitting horns with eachother.",
-        gridPosition: "six",
-        cardLayout: "normal",
-        urlSlug: "bootcamp-week2-css-naming-conventions-and-code-conflicts"
-    }
-
-]
+  {
+    id: "12",
+    name: "Designing Pinpoint",
+    date: "31 January 2025",
+    shortDescription:
+      "Designing lays a strong foundation for user experience, helping to define a project's needs and features. This week I jumped into Figma...",
+    thumbnailImg: "/assets/blog-imgs/wireframe.png",
+    altText: "A notepad with a hand drawn wireframe of a website.",
+    gridPosition: "two",
+    cardLayout: "normal",
+    urlSlug: "designing-pinpoint",
+  },
+  {
+    id: "13",
+    name: "It's fine to prop drill",
+    date: "3 March 2025",
+    shortDescription:
+      "It’s fine to prop drill - at least when building Pinpoint. Since it’s a small application with only one or two layers...",
+    thumbnailImg: "/assets/blog-imgs/Drilling.png",
+    altText: "A person using a drill.",
+    gridPosition: "three",
+    cardLayout: "normal",
+    urlSlug: "fine-prop-drilling",
+  },
+  {
+    id: "14",
+    name: "From Marketing to Frontend Development",
+    date: "23 June 2025",
+    shortDescription:
+      "It’s been just over a year since leaving my marketing role and embarking on a career switch to frontend development, and I wanted to take some time to reflect...",
+    thumbnailImg: "/assets/blog-imgs/cat-code.png",
+    altText: "A cat sitting infront of a Mac screen with code on it.",
+    gridPosition: "four",
+    cardLayout: "normal",
+    urlSlug: "marketing-to-frontend-development",
+  },
+  {
+    id: "15",
+    name: "Bootcamp Week 1 – Learning Agile and Pair Programming",
+    date: "19 September 2025",
+    shortDescription:
+      "This week, I completed the first week of TechNative Digital’s Software Development – Tech Finishing School Bootcamp, a 12-week program designed to take coding skills to a job-ready level.",
+    thumbnailImg: "/assets/blog-imgs/pears.png",
+    altText: "Two pears",
+    gridPosition: "five",
+    cardLayout: "wide",
+    urlSlug: "bootcamp-week1-agile-and-pair-programming",
+  },
+  {
+    id: "16",
+    name: "Bootcamp Week 2 – CSS Naming Conventions and Code Conflicts",
+    date: "26 September 2025",
+    shortDescription:
+      "This week’s bootcamp focus was on CSS and pair programming. We started with a solo CSS tutorial, which served as a great refresher...",
+    thumbnailImg: "/assets/blog-imgs/conflict-deer.png",
+    altText: "Two deers hitting horns with eachother.",
+    gridPosition: "six",
+    cardLayout: "wide",
+    urlSlug: "bootcamp-week2-css-naming-conventions-and-code-conflicts",
+  },
+  {
+    id: "17",
+    name: "Bootcamp Week 3 – JavaScript and Peer Learning",
+    date: "3 October 2025",
+    shortDescription:
+      "This week’s bootcamp centered on JavaScript and research. On day one, we independently built an interactive web page, using JavaScript...",
+    thumbnailImg: "/assets/blog-imgs/learning.png",
+    altText: "A post it note that says keep learning",
+    gridPosition: "seven",
+    cardLayout: "normal",
+    urlSlug: "bootcamp-week3-javascript-and-peer-learning",
+  },
+];

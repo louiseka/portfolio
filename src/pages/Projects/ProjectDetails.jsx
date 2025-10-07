700c21722bb99ca1c661685595e34a11a497eb2e
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-07 16:01:41 +0100

    Adding skills to project overviews

--- src/pages/Projects/ProjectDetails.jsx
+++ src/pages/Projects/ProjectDetails.jsx
@@ -1,66 +1,95 @@
-import { useParams, Link } from "react-router"
+import { useParams, Link } from "react-router";
 import { useEffect } from "react";
-import { projectData } from "../../data"
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa"
-
+import { projectData } from "../../data";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 export default function ProjectDetails() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  const { urlSlug } = useParams();
 
-    const { urlSlug } = useParams()
+  function getSkillClass(index) {
+    const n = index % 3;
 
-    function getSkillClass(index) {
-
-        const n = index % 3
-
-
-        if (n === 0) {
-            return "green-label"
-        }
-        if (n === 1) {
-            return "pink-label"
-        }
-        if (n === 2) {
-            return "purple-label"
-        }
+    if (n === 0) {
+      return "green-label";
     }
+    if (n === 1) {
+      return "pink-label";
+    }
+    if (n === 2) {
+      return "purple-label";
+    }
+  }
 
-    const projectDetails = projectData.find((project) => {
-        return project.urlSlug === urlSlug
-    })
+  const projectDetails = projectData.find((project) => {
+    return project.urlSlug === urlSlug;
+  });
 
-    const projectSkills = projectDetails.skills.map((skill, index) => {
-        return (
-            <p key={index} className={getSkillClass(index)}>{skill}</p>
-        )
-    })
-
+  const projectSkills = projectDetails.skills.map((skill, index) => {
     return (
-        <>
-            <title>{`${projectDetails.name} - Projects - Louise Aldridge`}</title>
-            <main key={projectDetails.id} className="project-details">
-                <Link className="back-link" to={"/#projects"} title="An overview page of Louise Aldridge's projects"><FaArrowLeft className="icon" />
-                    Back to all projects</Link>
-                <h1 className="heading">{projectDetails.name}</h1>
-                <div className="project-imgs-section">
-                    <img className="project-img" src={projectDetails.img1} alt={projectDetails.img1Alt} />
-                    <img className="project-img" src={projectDetails.img2} alt={projectDetails.img2Alt} />
-                    <img className="project-img" src={projectDetails.img3} alt={projectDetails.img3Alt} />
-                </div>
+      <p key={index} className={getSkillClass(index)}>
+        {skill}
+      </p>
+    );
+  });
 
-                <h2>Overview</h2>
-                <p className="overview-text">{projectDetails.longDescription}</p>
-                <Link className="cta-link" to={projectDetails.githubUrl} target="_blank" title={`GitHub repository for ${projectDetails.name}`}>Visit GitHub Repository <FaArrowRight className="icon" /></Link>
-                {projectDetails.liveUrl != "" && <Link className="cta-link" to={projectDetails.liveUrl} target="_blank" title={`The live website for ${projectDetails.name}`} >Visit live application <FaArrowRight className="icon" /></Link>}
+  return (
+    <>
+      <title>{`${projectDetails.name} - Projects - Louise Aldridge`}</title>
+      <main key={projectDetails.id} className="project-details">
+        <Link
+          className="back-link"
+          to={"/#projects"}
+          title="An overview page of Louise Aldridge's projects"
+        >
+          <FaArrowLeft className="icon" />
+          Back to all projects
+        </Link>
+        <h1 className="heading">{projectDetails.name}</h1>
+        <div className="project-imgs-section">
+          <img
+            className="project-img"
+            src={projectDetails.img1}
+            alt={projectDetails.img1Alt}
+          />
+          <img
+            className="project-img"
+            src={projectDetails.img2}
+            alt={projectDetails.img2Alt}
+          />
+          <img
+            className="project-img"
+            src={projectDetails.img3}
+            alt={projectDetails.img3Alt}
+          />
+        </div>
 
-                <div className="skills-container">
-                    {projectSkills}
-                </div>
+        <h2>Overview</h2>
+        <p className="overview-text">{projectDetails.longDescription}</p>
+        <Link
+          className="cta-link"
+          to={projectDetails.githubUrl}
+          target="_blank"
+          title={`GitHub repository for ${projectDetails.name}`}
+        >
+          Visit GitHub Repository <FaArrowRight className="icon" />
+        </Link>
+        {projectDetails.liveUrl != "" && (
+          <Link
+            className="cta-link"
+            to={projectDetails.liveUrl}
+            target="_blank"
+            title={`The live website for ${projectDetails.name}`}
+          >
+            Visit live application <FaArrowRight className="icon" />
+          </Link>
+        )}
 
-            </main>
-        </>
-    )
+        <div className="skills-container">{projectSkills}</div>
+      </main>
+    </>
+  );
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,50 +1,92 @@
-import { projectData } from "../../data"
-import { Link } from "react-router"
-import { FaArrowRight } from "react-icons/fa"
-
+import { projectData } from "../../data";
+import { Link } from "react-router";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Projects() {
+  const gridBackground = {
+    one: "green-bg",
+    two: "pink-bg",
+    three: "purple-bg",
+    four: "green-bg",
+    five: "pink-bg",
+    six: "green-bg",
+    seven: "purple-bg",
+  };
 
-    const projectElements = projectData.map(project => {
+  function getSkillClass(index, backgroundColour) {
+    const labelClasses = ["green-label", "pink-label", "yellow-label"];
+    let label = labelClasses[index % labelClasses.length];
 
-        const isWide = project.cardLayout === "wide"
+    switch (backgroundColour) {
+      case "green-bg":
+        label = "pink-label";
+        break;
+      case "pink-bg":
+        label = "green-label";
+        break;
+      case "purple-bg":
+        label = "yellow-label";
+        break;
+      default:
+        label = labelClasses[index % labelClasses.length];
+    }
+    return label;
+  }
 
-        const projectName = project.name
-        const projectShortDesc = project.shortDescription
-        const projectThumbnail = project.thumbnailImg
-        const projectAltText = project.img1Alt
-        const projectGridPosition = `${project.gridPosition} card `
+  const projectElements = projectData.map((project) => {
+    const isWide = project.cardLayout === "wide";
+    const backgroundColour = gridBackground[project.gridPosition];
 
+    const projectSkills = project.skills.map((skill, index) => {
+      return (
+        <p key={index} className={getSkillClass(index, backgroundColour)}>
+          {skill}
+        </p>
+      );
+    });
 
-        return (
-            <section key={project.id} className={projectGridPosition}>
-                <div className={isWide ? "wide-inner-content" : ""}>
-                    <img className={isWide ? "wide-grid-img" : "normal-grid-img"} alt={projectAltText} src={projectThumbnail} />
-                    <div className={isWide ? "wider-inner-text" : ""}>
-                        <h3 className="project-title">{projectName}</h3>
-                        <p className="project-desc">{projectShortDesc}</p>
-                        <Link className="small-cta-link" to={`projects/${project.urlSlug}`} title={`More details about ${projectName}`} key={project.id}>Find out more <FaArrowRight className="icon" /> </Link>
-                    </div>
-                </div>
-            </section >
-        )
-
-
-
-    })
+    const projectName = project.name;
+    const projectShortDesc = project.shortDescription;
+    const projectThumbnail = project.thumbnailImg;
+    const projectAltText = project.img1Alt;
+    const projectGridPosition = `${project.gridPosition} card `;
 
     return (
-        <main className="projects-grid" id="projects">
-            <h2>My portfolio of projects</h2>
-            <p className="sub-text"> While learning the fundamentals of frontend development, I have been practicing my skills and applying the languages and frameworks I've learned by creating various projects. Over time, I’ve also revisited these projects to improve them with my new knowledge. </p>
-            <div className="wrapper">
-                {projectElements}
-            </div>
-        </main>
+      <section key={project.id} className={projectGridPosition}>
+        <div className={isWide ? "wide-inner-content" : ""}>
+          <img
+            className={isWide ? "wide-grid-img" : "normal-grid-img"}
+            alt={projectAltText}
+            src={projectThumbnail}
+          />
+          <div className={isWide ? "wider-inner-text" : ""}>
+            <h3 className="project-title">{projectName}</h3>
+            <p className="project-desc">{projectShortDesc}</p>
+            <Link
+              className="small-cta-link"
+              to={`projects/${project.urlSlug}`}
+              title={`More details about ${projectName}`}
+              key={project.id}
+            >
+              Find out more <FaArrowRight className="icon" />{" "}
+            </Link>
+            <div className="project-skills-container">{projectSkills}</div>
+          </div>
+        </div>
+      </section>
+    );
+  });
 
-    )
-
-
-
-
+  return (
+    <main className="projects-grid" id="projects">
+      <h2>My portfolio of projects</h2>
+      <p className="sub-text">
+        While learning the fundamentals of frontend development, I have been
+        practicing my skills and applying the languages and frameworks I've
+        learned by creating various projects. Over time, I’ve also revisited
+        these projects to improve them with my new knowledge.{" "}
+      </p>
+      <div className="wrapper">{projectElements}</div>
+    </main>
+  );
 }
